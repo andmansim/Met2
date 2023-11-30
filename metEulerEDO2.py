ec870df03013d9cb9145a1caebfac6054049e661
andmansim <91722324+andmansim@users.noreply.github.com>
2023-11-30 18:49:55 +0100

    baab

---
+++ metEulerEDO2.py
@@ -0,0 +1,75 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def funcion(x,y):
+    return (-2*x*(np.exp(x**2)*y-1))/(np.exp(x**2))
+def iterar(x, y, f): 
+    '''Itera la función'''
+    while x <= xf:
+        xn = x + h
+        yn = y + h * f (x,y)
+        iterar(xn, yn, f)
+        puntos.append((xn, yn))
+        print(xn, yn)
+        return xn, yn
+
+
+def pintar(puntos):
+    '''Pinta la gráfica'''
+    x = []
+    y = []
+    for i in puntos:
+        x.append(i[0])
+        y.append(i[1])
+    plt.plot(x, y)
+    plt.show()
+
+
+#Código para pintar las soluciones particulares
+def iterar_solu(x,f): 
+    '''Itera la función'''
+    while x <= xf:
+        xn = x + h
+        yn =  h * f (x)
+        iterar_solu(xn, f)
+        puntos_solus.append((xn, yn))
+        print(xn, yn)
+        return xn, yn
+
+
+def funcion1(x):
+    return (x**2)/(np.exp(x**2))
+def funcion2(x):
+    return (x**2 + 1)/(np.exp(x**2))
+def funcion3(x):
+    return ((x**2) -1)/(np.exp(x**2))
+
+
+
+#main
+x0 = float(input('Introduce la x inicial: ')) #punto inicial
+y0 = float(input('Introduce la y inicial: ')) #punto inicial
+xf = float(input('Introduce el extremo final: ')) #punto final
+n = int(input('Número de divisiones: '))
+h = (xf - x0)/n #intervalo pequeño
+puntos = [] #lista de puntos
+puntos_solus=[] #lista de puntos de las soluciones particulares
+
+iterar(x0, y0, funcion)
+pintar(puntos)
+
+#funciones particulares de la solución real
+iterar_solu(x0, funcion1)
+pintar(puntos_solus)
+puntos_solus=[]
+iterar_solu(x0, funcion2)
+pintar(puntos_solus)
+puntos_solus=[]
+iterar_solu(x0, funcion3)
+pintar(puntos_solus)
+
+
+    
+
+
+    
