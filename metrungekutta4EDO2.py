fa926bcb1e8b94bec3264ac4b1b5da1d72da8976
andmansim <91722324+andmansim@users.noreply.github.com>
2023-12-05 17:34:17 +0100

    ya

--- metrungekutta4EDO2.py
+++ metrungekutta4EDO2.py
@@ -7,9 +7,13 @@ a y b dependen de xi, vi, ui y f(xi)
 import matplotlib.pyplot as plt
 import numpy as np
 
-def funcion(x,y):
-    return (-2*x*(np.exp(x**2)*y-1))/(np.exp(x**2))
-def iterar(x, y, f): 
+def funcion(x, u, v):
+    '''la función es y'' + a*y' + b*y = f(x)'''
+    #donde a y b dependen de x
+    #u(x)=y(x) y v(x)=y'(x)
+    #sería (-b * u -a * v + f(x)) = y''(x),  donde sustituimos b, a y f(x)
+    return (-u - (v/x))
+def iterar(x, u, v, f): 
     '''Itera la función'''
     while x <= xf:
         #constantes (pesos)
@@ -19,24 +23,25 @@ def iterar(x, y, f):
         a4 = 1/6
         
         #Pendientes
-        (k11, k12) = [[0, 1], [-b(x), -a(x)]]*[[u], [v]] + [0, f(x)]
-        (k21, k22) = [[0, 1], [-b(xn), -a(xn)]]*[[u + (h/2)*k11], [v + (h/2)*k12]] + [0, f(x +(h/2))]
-        (k31, k32) = [[0, 1], [-b(xn), -a(xn)]]*[[u + (h/2)*k21], [v + (h/2)*k22]] + [0, f(x +(h/2))]
-        (k41, k42) = [[0, 1], [-b(xn), -a(xn)]]*[[u + h*k31], [v + h*k32]] + [0, f(x +h)]
+        k11 = v
+        k12 = f(x, u, v) 
+        k21 = v + (h/2)*k11
+        k22 = f(x +(h/2), u + (h/2)*k11, v + (h/2)*k12)
+        k31 = v + (h/2)*k21
+        k32 = f(x +(h/2), u + (h/2)*k21, v + (h/2)*k22)
+        k41 = v + h*k31
+        k42 = f(x +h, u + h*k31, v + h*k32)
         
-        k2 = f(x +(h/2), y + (h/2)*k1)
-        k3 = f(x +(h/2), y + (h/2)*k2)
-        k4 = f(x +h, y + h*k3)
         
         #función
         xn = x + h
         un = u + h*(a1*k11 + a2*k21 + a3*k31 + a4*k41)
         vn = v + h*(a1*k12 + a2*k22 + a3*k32 + a4*k42) 
         
-        iterar(xn, yn, f)
-        puntos.append((xn, yn))
-        print(xn, yn)
-        return xn, yn
+        iterar(xn, un,vn, f)
+        puntos.append((xn, un))
+        print(xn, un)
+        return xn, un
 
 
 def pintar(puntos):
