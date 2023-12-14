715e4739e85958148e4b7b0b22947eff228bd255
andmansim <91722324+andmansim@users.noreply.github.com>
2023-12-14 20:16:38 +0100

    mejor

--- metTaylor.py
+++ metTaylor.py
@@ -3,18 +3,24 @@ import numpy as np
 
 def funcion(x,y):
     return (2-3*x -y)/(x-1)
+
 def segunda_derivada(x, y):
     return (3*x + 2*y -1)/(x-1)**2
 
 def iterar(x, y, f, d2): 
     '''Itera la función'''
-    while x <= xf:
-        xn = x + h
-        yn = y + h * f (x,y) + (h**2/2) * d2(x,y)
-        iterar(xn, yn, f, d2)
-        puntos.append((xn, yn))
-        print(xn, yn)
-        return xn, yn
+    if x > xf:
+        return x, y
+    
+    xn = x + h
+    yn = y + h * f (x,y) + (h**2/2) * d2(x,y)
+    
+    iterar(xn, yn, f, d2)
+    puntos.append((xn, yn))
+    
+    print(xn, yn)
+    
+    return xn, yn
 
 
 def pintar(puntos):
@@ -37,5 +43,3 @@ puntos = [] #lista de puntos
 
 iterar(x0, y0, funcion, segunda_derivada)
 pintar(puntos)
-# Con esta función (2-3*x -y)/(x-1) tenemos, solución real es y(6) = -8,20 y la solución aproximada es w100 = -8.1997 --> tenemos un error de 0.0003
-# Con esta función (1+4*x*y)/(3*x**2) tenemos, solución real es y(4)= -11,46 y la solución aproximada es w100 = -11.47088 --> tenemos un error de 0.01088
